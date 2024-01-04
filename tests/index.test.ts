6e46a7f8bfea40a1831e47286d558a0ae4e51a3f
yutak23 <yutapilafu@gmail.com>
2024-01-04 18:21:35 +0900

    chore: update README and fix format

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,5 +1,5 @@
 import { describe, it, expect } from 'vitest';
-import example from "../src/index.js";
+import example from '../src/index.js';
 
 describe('Test', () => {
 	it('req.token is set', () => {
